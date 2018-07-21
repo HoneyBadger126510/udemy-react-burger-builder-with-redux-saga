b20178907e3fd7b998f21adac91d01788c205b3d
jtawes <[jtawes510@gmail.com]>
2018-07-21 18:30:39 -0400

    access env variable to make redux dev tools available only in dev env

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -7,7 +7,7 @@ import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import * as actions from '../../store/actions/index';
-import { updateObject } from '../../../shared/utility';
+import { updateObject } from '../../shared/utility';
 
 class Auth extends Component {
   state = {
@@ -87,7 +87,7 @@ class Auth extends Component {
         value: event.target.value,
         valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
         touched: true
-      } );
+      } )
     } );
     this.setState( { controls: updatedControls } );
   }
